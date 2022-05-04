2a6263626bcdba066053c83154edb84ba28027e4
PJ <pj>
2022-05-04 18:04:35 +0200

    fixed tests

--- apps/nodejs/tests/sita-client.test.ts
+++ apps/nodejs/tests/sita-client.test.ts
@@ -1,9 +1,9 @@
 import { getAccessToken } from '../sita-client';
 
+
 describe("test authentication", () => {
     it("should get jwt token", async () => {
         const token = await getAccessToken();
-        console.log(token);
-        // expect(token.length).toBeGreaterThan(0);
+        expect(token.length).toBeGreaterThan(0);
     })
 })
