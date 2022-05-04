db7c10de7682c00bae086f0a2e54129c3de1f740
PJ <pj>
2022-05-04 18:28:47 +0200

    error handling

--- apps/nodejs/sita-client.ts
+++ apps/nodejs/sita-client.ts
@@ -2,20 +2,26 @@ import axios from 'axios'
 import qs from 'qs'
 
 export async function getAccessToken(): Promise<string> {
-    const x = await axios({
-        method: 'post',
-        url: `https://login.microsoftonline.com/${process.env.AAD_TENANT}.onmicrosoft.com/oauth2/token`,
-        data: qs.stringify({
-            grant_type: 'client_credentials',
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET,
-            resource: process.env.RESOURCE
-        }),
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-        }
-    })
-    return x.data.access_token
+    try {
+        const x = await axios({
+            method: 'post',
+            url: `https://login.microsoftonline.com/${process.env.AAD_TENANT}.onmicrosoft.com/oauth2/token`,
+            data: qs.stringify({
+                grant_type: 'client_credentials',
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+                resource: process.env.RESOURCE
+            }),
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+        })
+        return x.data.access_token
+    } catch(e) {
+        console.log(e)
+    }
+    return ''
+
 }
 
 export async function createSubscription(accessToken: string): Promise<Subscription> {
--- apps/nodejs/tests/sita-client.test.ts
+++ apps/nodejs/tests/sita-client.test.ts
@@ -1,4 +1,4 @@
-import { getAccessToken } from '../sita-client';
+import { createSubscription, getAccessToken } from '../sita-client';
 const dotenv = require('dotenv')
 
 dotenv.config()
@@ -7,5 +7,12 @@ describe("test authentication", () => {
     it("should get jwt token", async () => {
         const token = await getAccessToken();
         expect(token.length).toBeGreaterThan(0);
+    })
+})
+describe("test subscription", () => {
+    it("should create subscription", async () => {
+        const token = await getAccessToken();
+        // const sub = await createSubscription(token);
+        // expect(sub.subscriptionId).toBeGreaterThan(0);
     })
 })
