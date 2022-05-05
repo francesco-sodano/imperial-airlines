95781b6726c6a1b0dc2bb95244023876e1fc3246
PJ <pj>
2022-05-05 14:55:12 +0200

    latest changes

--- apps/nodejs/server.ts
+++ apps/nodejs/server.ts
@@ -1,23 +1,32 @@
-import { createConnection, createReservation, createSubscription, getAccessToken } from './sita-client';
+import {
+    createConnection,
+    createReservation,
+    createSubscription,
+    getAccessToken,
+    startScan,
+    stopScan
+} from './sita-client';
 import { createSbClient } from './service-bus';
-
 const express = require('express')
 const dotenv = require('dotenv')
 
 dotenv.config()
 
 const app = express()
+app.use('/',express.static('html'))
 const port = process.env.PORT || 3000
-
 
 let sbClient = undefined
 
 const messageHandler = async (messageReceived: any) => {
-    console.log(`Received message: ${messageReceived.body}`);
+    console.log(`Received message: ${JSON.stringify(messageReceived.body)}`);
+    
 };
 
+let connectionId = ''
+let passportName = ''
 
-app.get('/', async(req:Express.Request, res: Response) => {
+app.get('/startScan', async(req:Express.Request, res: Response) => {
     const token = await getAccessToken()
     const subscription = await createSubscription(token)
     const reservation = await createReservation(token, process.env.LOCATION_SCANNER || '')
@@ -26,12 +35,26 @@ app.get('/', async(req:Express.Request, res: Response) => {
         subscription.subscriptionId,
         reservation.reservationId,
         'OCR1')
+    connectionId = connection.connectionId
+    const startScanResponse = await startScan(token, connection.connectionId);
     // @ts-ignore
-    res.status(200).json(connection)
+    res.status(200).json({'scanId': connectionId })
 });
 
+app.get('/stopScan', async(req:Express.Request, res: Response) => {
+    const token = await getAccessToken()
+    await stopScan(token, connectionId);
+    // @ts-ignore
+    res.status(200).json({'status': 'ok'})
+})
+
+app.get('/getPassport', async(req:Express.Request, res: Response) => {
+    // @ts-ignore
+    res.status(200).json({passportName})
+})
+
 app.listen(port, async() => {
-    console.log(`[server]: Server is running at https://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
     const token = await getAccessToken()
     const subscription = await createSubscription(token)
     console.log('Initial subscription', subscription)
